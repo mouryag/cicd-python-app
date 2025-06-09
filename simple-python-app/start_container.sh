32a4e9b9b7072f1e660c1f3efbcdd99cb54a3850
mouryag <mouryag99@gmail.com>
2025-06-09 23:51:17 +0530

    add files for code-deploy

--- simple-python-app/start_container.sh
+++ simple-python-app/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull mgudipudi/simple-python-flask-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 mgudipudi/simple-python-flask-app
